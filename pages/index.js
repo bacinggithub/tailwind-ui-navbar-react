192d8911538fd59ffd3f155dd5e909de31eae14f
Ibrahim <bacing88@gmail.com>
2020-08-09 01:45:15 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -83,7 +83,7 @@ const Navbar = () => {
                           d="M3 12l9-9 9 9M5 10v10a1 1 0 001 1h3a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1h3a1 1 0 001-1V10M9 21h6"
                         />
                       </svg>
-                      Dashboard
+                      DashboardYyyyy
                     </a>
                     <a
                       href="#"
